3f2674be1a5ac57964e79f2de3362354128fae55
adminmajkel <michal@frontcode.pl>
2019-01-28 00:18:25 +0100

    Overlay spinner

--- front/src/assets/scripts/scripts.tsx
+++ front/src/assets/scripts/scripts.tsx
@@ -29,52 +29,12 @@ const container = document.getElementById("app");
 
 
 
+enum OverlayStatus {
+    open = 'is-open',
+    closed = 'is-closed',
+    loading = 'is-loading'
+}
 
-
-
-
-const ease = {
-    exponentialIn: (t) => {
-        return t == 0.0 ? t : Math.pow(2.0, 10.0 * (t - 1.0));
-    },
-    exponentialOut: (t) => {
-        return t == 1.0 ? t : 1.0 - Math.pow(2.0, -10.0 * t);
-    },
-    exponentialInOut: (t) => {
-        return t == 0.0 || t == 1.0
-            ? t
-            : t < 0.5
-                ? +0.5 * Math.pow(2.0, (20.0 * t) - 10.0)
-                : -0.5 * Math.pow(2.0, 10.0 - (t * 20.0)) + 1.0;
-    },
-    sineOut: (t) => {
-        const HALF_PI = 1.5707963267948966;
-        return Math.sin(t * HALF_PI);
-    },
-    circularInOut: (t) => {
-        return t < 0.5
-            ? 0.5 * (1.0 - Math.sqrt(1.0 - 4.0 * t * t))
-            : 0.5 * (Math.sqrt((3.0 - 2.0 * t) * (2.0 * t - 1.0)) + 1.0);
-    },
-    cubicIn: (t) => {
-        return t * t * t;
-    },
-    cubicOut: (t) => {
-        const f = t - 1.0;
-        return f * f * f + 1.0;
-    },
-    cubicInOut: (t) => {
-        return t < 0.5
-            ? 4.0 * t * t * t
-            : 0.5 * Math.pow(2.0 * t - 2.0, 3.0) + 1.0;
-    },
-    quadraticOut: (t) => {
-        return -t * (t - 2.0);
-    },
-    quarticOut: (t) => {
-        return Math.pow(t - 1.0, 3.0) * (1.0 - t) + 1.0;
-    },
-};
 
 interface OverlayProps {
     status: string
@@ -86,48 +46,54 @@ interface OverlayState {
 
 export class AppOverlay extends React.Component<OverlayProps, OverlayState> {
     private path = [];
-    private numPoints = 2;
-    private duration = 300;
-    private delayPointsArray = [];
-    private delayPointsMax = 0;
-    private delayPerPath = 50;
+    private duration = 500;
     private timeStart = Date.now();
     private isOpened = false;
-    private isAnimating = false;
-
-    private pathsRef: any;
+    private spinner: HTMLElement;
+    readonly spinnerRef: any;
+    readonly pathsRef: any;
 
     constructor (props) {
         super(props);
         this.state = {
-            status: ""
+            status: this.props.status
         };
         this.pathsRef = React.createRef();
+        this.spinnerRef = React.createRef();
     }
 
-    componentDidUpdate(nextProps: Readonly<OverlayProps>, nextContext: any): void {
-        if (nextProps.status !== this.props.status) {
-            this.toggle();
+    componentDidUpdate(prevProps: Readonly<OverlayProps>, prevState: Readonly<OverlayState>, snapshot?: any): void {
+        if (prevProps.status !== this.props.status) {
+            if (this.props.status === OverlayStatus.open) {
+                this.open();
+            }
+            if (this.props.status === OverlayStatus.closed) {
+                this.close();
+            }
+            if (this.props.status === OverlayStatus.loading) {
+                this.showSpinner();
+            }
         }
     }
 
     componentDidMount(): void {
         this.path = this.pathsRef.current.childNodes;
+        this.spinner = this.spinnerRef.current;
     }
 
-    toggle() {
-        this.isAnimating = true;
-        for (var i = 0; i < this.numPoints; i++) {
-            this.delayPointsArray[i] = 0;
-        }
-        if (this.isOpened === false) {
-            this.open();
-        } else {
-            this.close();
-        }
+    hideSpinner (): void {
+        this.spinner.style.display = 'none';
     }
+
+    showSpinner (): void {
+        this.spinner.style.display = 'block';
+    }
+
     open() {
         this.isOpened = true;
+        this.hideSpinner();
+        this.path[0].setAttribute('cx', 0);
+        this.path[0].setAttribute('cy', 0);
         this.setState({
             status: this.props.status
         }, () => {
@@ -138,61 +104,52 @@ export class AppOverlay extends React.Component<OverlayProps, OverlayState> {
     close() {
         this.isOpened = false;
         this.timeStart = Date.now();
+        this.hideSpinner();
+        this.path[0].setAttribute('cx', 350);
+        this.path[0].setAttribute('cy', 250);
         this.renderLoop();
         setTimeout(() => {
             this.setState({
                 status: this.props.status
             });
         }, 500);
-
     }
-    updatePath(time) {
-        const points = [];
-        for (var i = 0; i < this.numPoints; i++) {
-            const thisEase = this.isOpened ?
-                (i == 1) ? ease.cubicOut : ease.cubicInOut:
-                (i == 1) ? ease.cubicInOut : ease.cubicOut;
-            points[i] = thisEase(Math.min(Math.max(time - this.delayPointsArray[i], 0) / this.duration, 1)) * 100
-        }
 
-        let str = '';
-        str += (this.isOpened) ? `M 0 0 V ${points[0]} ` : `M 0 ${points[0]} `;
-        for (var i = 0; i < this.numPoints - 1; i++) {
-            const p = (i + 1) / (this.numPoints - 1) * 100;
-            const cp = p - (1 / (this.numPoints - 1) * 100) / 2;
-            str += `C ${cp} ${points[i]} ${cp} ${points[i + 1]} ${p} ${points[i + 1]} `;
-        }
-        str += (this.isOpened) ? `V 0 H 0` : `V 100 H 0`;
-        return str;
-    }
     renderFrame() {
         if (this.isOpened) {
-            for (var i = 0; i < this.path.length; i++) {
-                this.path[i].setAttribute('d', this.updatePath(Date.now() - (this.timeStart + this.delayPerPath * i)));
-            }
+            const newSize = Date.now() - this.timeStart;
+            this.path[0].setAttribute('r', newSize >= 0 ? newSize : 0);
         } else {
-            for (var i = 0; i < this.path.length; i++) {
-                this.path[i].setAttribute('d', this.updatePath(Date.now() - (this.timeStart + this.delayPerPath * (this.path.length - i - 1))));
-            }
+            const newSize = this.duration - (Date.now() - this.timeStart);
+            this.path[0].setAttribute('r', newSize >= 0 ? newSize : 0);
         }
     }
     renderLoop() {
         this.renderFrame();
-        if (Date.now() - this.timeStart < this.duration + this.delayPerPath * (this.path.length - 1) + this.delayPointsMax) {
+        if (Date.now() - this.timeStart < this.duration) {
             requestAnimationFrame(() => {
                 this.renderLoop();
             });
-        }
-        else {
-            this.isAnimating = false;
         }
     }
 
     render () {
         return (
             <div className="app-overlay">
-                <svg className={"shape-overlays " + this.state.status} ref={this.pathsRef} viewBox="0 0 100 100" preserveAspectRatio="none">
-                    <path className="shape-overlays__path"/>
+                <svg className={"shape-overlays " + this.state.status} ref={this.pathsRef} viewBox="0 0 250 250" preserveAspectRatio="none">
+                    <circle cx="0" cy="0" r="0" className="shape-overlays__path"/>
+                </svg>
+                <svg className={"shape-overlays-loader"} version="1.1" ref={this.spinnerRef} x="0px" y="0px"
+                     width="40px" height="40px" viewBox="0 0 50 50">
+                    <path fill="#000" d="M25.251,6.461c-10.318,0-18.683,8.365-18.683,18.683h4.068c0-8.071,6.543-14.615,14.615-14.615V6.461z">
+                        <animateTransform attributeType="xml"
+                                          attributeName="transform"
+                                          type="rotate"
+                                          from="0 25 25"
+                                          to="360 25 25"
+                                          dur="0.6s"
+                                          repeatCount="indefinite"/>
+                    </path>
                 </svg>
             </div>
         );
@@ -246,30 +203,38 @@ export class AppContent extends React.Component<ComponentProps, ComponentState> 
         this.state = {
             currentRouteId: this.getCurrentRouteId(this.props.routes.active),
             pageStatus: 'loaded',
-            overlayStatus: 'is-closed'
+            overlayStatus: OverlayStatus.closed
         }
     }
 
     componentWillUpdate(nextProps: Readonly<ComponentProps>, nextState: Readonly<ComponentState>, nextContext: any): void {
+        const runOverlayLoadingAnimation = () => {
+            setTimeout( () => {
+                this.setState({
+                    pageStatus: 'loaded',
+                    overlayStatus: OverlayStatus.closed
+                });
+                window.scrollTo(0, 0);
+            }, 500);
+        };
+
+        const runOverlayOpenAnimation = () => {
+            setTimeout(() => {
+                this.setState({
+                    pageStatus: 'loading',
+                    overlayStatus: OverlayStatus.loading
+                }, () => {
+                    this.updateCurrentId(nextProps.routes.active, runOverlayLoadingAnimation);
+                });
+            }, 300);
+        };
+
         if (nextProps.routes.active !== this.props.routes.active) {
             this.setState({
                 pageStatus: 'out',
-                overlayStatus: 'is-open'
+                overlayStatus: OverlayStatus.open
             }, () => {
-                setTimeout(() => {
-                    this.setState({
-                        pageStatus: 'loading'
-                    });
-                    this.updateCurrentId(nextProps.routes.active, () => {
-                        setTimeout( () => {
-                            this.setState({
-                                pageStatus: 'loaded',
-                                overlayStatus: 'is-closed'
-                            });
-                            window.scrollTo(0, 0);
-                        }, 200);
-                    });
-                }, 300);
+                runOverlayOpenAnimation();
             });
         }
     }
